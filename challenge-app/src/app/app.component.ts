412948c2e716f57da349fa05ea67bee294cedb4e
pablobello-se <pablo.bello.se@gmail.com>
2025-02-11 18:19:53 -0300

    removes boilerplate code from the template. Adds provider for later HttpClient.

--- challenge-app/src/app/app.component.ts
+++ challenge-app/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'challenge-app';
+    title = 'challenge-app';
 }
